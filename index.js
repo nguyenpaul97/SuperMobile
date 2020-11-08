6e5ce6c0dce6012c17461583e5f9c6f2b04b8ca0
nguyenpaul97 <nguyenpaul.ic.1997@gmail.com>
2020-11-07 23:49:59 -0800

    Completed HTML template

--- index.js
+++ index.js
@@ -72,10 +72,11 @@ $(document).ready(function () {
   // Product Quantity Section
   let $qty_up = $('.qty .qty-up');
   let $qty_down = $('.qty .qty-down');
-  let $input = $('.qty .qty_input');
+  // let $input = $('.qty .qty_input');
 
   // Click on Qty Up button
   $qty_up.click(function (e) {
+    let $input = $(`.qty_input[data-id='${$(this).data('id')}']`);
     if ($input.val() >= 1 && $input.val() <= 9) {
       $input.val(function (i, oldval) {
         return ++oldval;
@@ -85,6 +86,7 @@ $(document).ready(function () {
 
   // Click on Qty Down button
   $qty_down.click(function (e) {
+    let $input = $(`.qty_input[data-id='${$(this).data('id')}']`);
     if ($input.val() > 1 && $input.val() <= 10) {
       $input.val(function (i, oldval) {
         return --oldval;
